c5216c8262a781516f30e6fecc8f077e073a6aea
PeterDauwe <bobo5290461@gmail.com>
2022-03-02 09:35:44 +0100

    update

---
+++ aries-calamares-config-btrfs/build-aries.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
+deletion1="../../aries_repo/x86_64/"
+name1="${PWD##*/}"
+
+rm $deletion1/$name1-22*.zst
+
+
+
+destiny="../../aries_repo/x86_64/"
+
+#destiny=$destination1
+
+makepkg
+
+echo "Moving created files to " $destiny
+echo "#############################################################################################"
+mv $search*pkg.tar.zst $destiny
+
+ls | grep -v "PKGBUILD\|build-aries.sh" | xargs rm -rf
+
+echo "Cleaning up"
+echo "#############################################################################################"
+echo "deleting unnecessary folders"
+echo "#############################################################################################"
+
+sh ../../aries_repo/update_repo_push.sh
+tput setaf 8
+echo "#############################################################################################"
+echo "###################                       build done                   ######################"
+echo "#############################################################################################"
+tput sgr0
